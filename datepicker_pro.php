6454d1d24b714bdff977e5562333202d33475686
kwikl3arn <44991804+kwikl3arn@users.noreply.github.com>
2019-04-03 09:46:00 +0530

    Create datepicker_pro.php

---
+++ datepicker_pro.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title></title>
+  <script
+    type="text/javascript"
+    src="http://code.jquery.com/jquery-1.7.1.js"
+    
+  ></script>
+  
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.js"></script>
+     <link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.17/themes/base/jquery-ui.css">
+      <link rel="stylesheet" type="text/css" href="http://static.jquery.com/ui/css/demo-docs-theme/ui.theme.css">
+
+  <style id="compiled-css" type="text/css">
+      body {
+  font-size: 8pt;
+  font-family: Verdana;
+  padding: 5px;
+}
+
+  </style>
+
+
+  <!-- TODO: Missing CoffeeScript 2 -->
+
+  <script type="text/javascript">
+
+
+ 
+      
+$(document).ready(function() {
+  var today = new Date();
+
+  var yrRange = today.getFullYear();
+  $("#txtFromDate").datepicker({
+    yearRange: "2019:" + yrRange,
+    changeMonth: true,
+    changeYear: true,
+	dateFormat:'dd-mm-yy',
+    stepMonths: false,
+	
+	//maxDate: "today",
+   // endDate: "today",
+    endDate: "<?php echo date('d-m-y');?>",
+    maxDate: "<?php echo date('d-m-y');?>",
+  
+	
+    onSelect: function(selected) {
+      $("#txtToDate").datepicker("option", "minDate", selected)
+    }
+  });
+  $("#txtToDate").datepicker({
+
+    yearRange: "2019:" + yrRange,
+    changeMonth: true,
+    changeYear: true,
+	dateFormat:'dd-mm-yy',
+    stepMonths: false,
+   endDate: "<?php echo date('d-m-y');?>",
+    maxDate: "<?php echo date('d-m-y');?>",
+    //maxDate: "today",
+   // endDate: "today",
+    onSelect: function(selected) {
+      $("#txtFromDate").datepicker("option", "maxDate", selected)
+    }
+  });
+});
+
+
+
+ 
+
+</script>
+
+</head>
+<body>
+    <br/> From: <input type="text" id="txtFromDate" /> To: <input type="text" id="txtToDate" />
+
+ <h1> http://jsfiddle.net/a8q4vn71/</h1>
+
+</body>
+</html>
